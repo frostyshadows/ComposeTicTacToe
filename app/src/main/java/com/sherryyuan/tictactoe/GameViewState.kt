1f036137466674ab87b56f3b84b8e50aa507f5b6
Sherry Yuan <frostyyshadows@gmail.com>
2021-05-09 11:09:16 -0700

    clicks finally working

--- app/src/main/java/com/sherryyuan/tictactoe/GameViewState.kt
+++ app/src/main/java/com/sherryyuan/tictactoe/GameViewState.kt
@@ -1,12 +1,29 @@
 package com.sherryyuan.tictactoe
 
-data class GameViewState(
-    val info: String = "",
-    val turn: Turn = Turn.X_PLAYER,
-    val squares: List<String?> = List(9) { null }
-)
+data class GameViewState (
+    val info: String = "Next player: X",
+    private var turn: Turn = Turn.X_PLAYER,
+    val squares: List<String> = List(9) { "" }
+) {
 
-enum class Turn(val displayText: String) {
-    X_PLAYER("Next player: X"),
-    Y_PLAYER("Next player:Y")
+    fun updateState(index: Int): GameViewState {
+        val updatedTurn = when (turn) {
+            Turn.X_PLAYER -> Turn.Y_PLAYER
+            Turn.Y_PLAYER -> Turn.X_PLAYER
+        }
+
+        val updatedInfo = "Next player: ${updatedTurn.label}"
+        val updatedSquares = squares.toMutableList()
+        updatedSquares[index] = turn.label
+        return GameViewState(
+            info = updatedInfo,
+            turn = updatedTurn,
+            squares = updatedSquares
+        )
+    }
+}
+
+enum class Turn(val label: String) {
+    X_PLAYER("X"),
+    Y_PLAYER("Y")
 }
--- app/src/main/java/com/sherryyuan/tictactoe/MainActivity.kt
+++ app/src/main/java/com/sherryyuan/tictactoe/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sherryyuan.tictactoe
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
@@ -8,17 +9,17 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyVerticalGrid
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Black
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sherryyuan.tictactoe.ui.theme.TicTacToeTheme
@@ -31,7 +32,7 @@ class MainActivity : ComponentActivity() {
             TicTacToeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Game(GameViewState())
+                    Game()
                 }
             }
         }
@@ -39,37 +40,49 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Square(selection: String?) {
+fun Square(selection: String, onClick: () -> Unit) {
     Button(
-        modifier = Modifier.fillMaxWidth().height(128.dp),
-        onClick = {},
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(128.dp),
         border = BorderStroke(1.dp, Black),
-        shape = RectangleShape
+        shape = RectangleShape,
+        enabled = selection.isBlank(),
+        onClick = onClick,
     ) {
-        Text(text = selection.orEmpty())
+        Text(
+            text = selection,
+            color = Black,
+        )
     }
 }
 
 @ExperimentalFoundationApi
 @Composable
-fun Board(squares: List<String?>) {
+fun Board(squares: List<String>, onClick: (Int) -> Unit) {
     LazyVerticalGrid(
         cells = GridCells.Fixed(3)
     ) {
-        itemsIndexed(squares) { _, selection ->
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Square(selection)
-            }
+        items(squares.size) { index ->
+            Square(squares[index]) { onClick(index) }
         }
     }
 }
 
 @ExperimentalFoundationApi
 @Composable
-fun Game(state: GameViewState) {
+fun Game() {
+    var state by remember { mutableStateOf(GameViewState()) }
+
+    fun handleClick(index: Int) {
+        state = state.updateState(index)
+    }
     Column {
-        Text(text = state.info)
-        Board(squares = state.squares)
+        Text(text = state.info, color = Black)
+        Board(
+            squares = state.squares,
+            onClick = ::handleClick,
+        )
     }
 }
 
@@ -78,9 +91,6 @@ fun Game(state: GameViewState) {
 @Composable
 fun DefaultPreview() {
     TicTacToeTheme {
-        Game(GameViewState())
+        Game()
     }
 }
-
-const val X = "X"
-const val O = "O"
